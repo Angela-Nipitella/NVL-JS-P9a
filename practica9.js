41a396d919639a85e9e27c440b036aee19cbcbd9
Angela Nipitella <nipitellaangela@gmail.com>
2019-09-17 17:10:23 +0200

    practica Array JS

---
+++ practica9.js
@@ -0,0 +1,16 @@
+/*Crea un array donde cada elemento sea un lista de dos valores.
+
+Indica la longitud del array
+Indica la longitud de uno de los elementos del array
+Muestra por pantalla el valor del tercer elemento de la lista del array. 
+Y dentro de ese tercer elemento, su segundo elemento.*/
+
+var frutas = ['manzana', 'banana', 'pera', 'fresa', 'mango', 'sandia'];
+
+alert(frutas.length);
+
+alert(frutas[2].length);
+
+alert(frutas[2]);
+
+alert(frutas[2][1]);
